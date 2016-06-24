a52d2fd47e1e9788c47ea30098327331595ca72d
yinxunzhi <yinxunzhi@che001.com>
2016-06-24 15:16:53 +0800

    RandomAccessFile的demo

---
+++ src/main/java/com/yin/myproject/demo/io/RandomAccessFileDemo.java
@@ -0,0 +1,30 @@
+package com.yin.myproject.demo.io;
+
+import java.io.RandomAccessFile;
+
+public class RandomAccessFileDemo {
+	public static void main(String[] args) {
+		new RandomAccessFileDemo().demo1();
+	}
+
+	private void demo1() {
+		try {
+			RandomAccessFile rf = new RandomAccessFile("rtest.dat", "rw");
+			for (int i = 0; i < 10; i++) {
+				rf.writeDouble(i * 1.415);
+			}
+			rf.close();
+			rf = new RandomAccessFile("rtest.dat", "rw");
+			rf.seek(5 * 8);
+			rf.writeDouble(47.001);
+			rf.close();
+			rf = new RandomAccessFile("rtest.dat", "rw");
+			for (int i = 0; i < 10; i++) {
+				System.out.println("Value " + i + ": " + rf.readDouble());
+			}
+			rf.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
