afb13c8dbdd70b937adc9cadb8f6025894673eed
yinxunzhi <yinxunzhi@che001.com>
2016-07-11 18:44:23 +0800

    增加CharArrayReader使用示例

--- src/main/java/com/yin/myproject/demo/io/reader/ReaderDemo.java
+++ src/main/java/com/yin/myproject/demo/io/reader/ReaderDemo.java
@@ -1,5 +1,6 @@
 package com.yin.myproject.demo.io.reader;
 
+import java.io.CharArrayReader;
 import java.io.FileReader;
 import java.io.Reader;
 import java.io.StringReader;
@@ -8,8 +9,9 @@ public class ReaderDemo {
 	final String classPath = ReaderDemo.class.getResource("/").getPath();
 
 	public static void main(String[] args) throws Exception {
-//		new ReaderDemo().fileReaderDemo();
-		new ReaderDemo().stringReader();
+		// new ReaderDemo().fileReaderDemo();
+		// new ReaderDemo().stringReader();
+		new ReaderDemo().charArrayReaderDemo();
 	}
 
 	public void fileReaderDemo() throws Exception {
@@ -33,8 +35,10 @@ public class ReaderDemo {
 		builder.append(
 				"由于涉事土地为农用地，根据我国《土地管理法》等相关规定，征用土地必须经相关政府部门批准，取得农用地转为建设用地批准文号后才能对土地实施征用。土地的征用必须由市、县人民政府组织实施。这意味着洪万来与东城房地产公司的代征地协议一开始就是非法的。");
 		builder.append("就在洪万来开始推土时，广东省国土厅通过卫片执法发现了这一问题，并要求阳东县国土局跟进处理。2008年10月25日，东阳县国土局同一天出具了两份土地行政处罚决定书。");
-		builder.append("面对处罚，洪万来按要求交了罚款，也复耕复绿，但他并未停止非法征地行为。数据显示，洪万来在被处罚之后依然将征地赔偿款支付给东城房地产公司。如2009年2月20日支付了135万元、2009年4月28日支付了144094元。");
-		builder.append("面对处罚，洪万来按要求交了罚款，也复耕复绿，但他并未停止非法征地行为。数据显示，洪万来在被处罚之后依然将征地赔偿款支付给东城房地产公司。如2009年2月20日支付了135万元、2009年4月28日支付了144094元.");
+		builder.append(
+				"面对处罚，洪万来按要求交了罚款，也复耕复绿，但他并未停止非法征地行为。数据显示，洪万来在被处罚之后依然将征地赔偿款支付给东城房地产公司。如2009年2月20日支付了135万元、2009年4月28日支付了144094元。");
+		builder.append(
+				"面对处罚，洪万来按要求交了罚款，也复耕复绿，但他并未停止非法征地行为。数据显示，洪万来在被处罚之后依然将征地赔偿款支付给东城房地产公司。如2009年2月20日支付了135万元、2009年4月28日支付了144094元.");
 		Reader reader = new StringReader(builder.toString());
 		char cbuf[] = new char[1024];
 		int len = 0;
@@ -43,5 +47,29 @@ public class ReaderDemo {
 			System.out.println(new String(cbuf));
 		}
 		reader.close();
+	}
+
+	public void charArrayReaderDemo() throws Exception{
+		char[] buf = new char[]{'1','2','3','4','5','6'};
+		Reader reader = new CharArrayReader(buf);
+		//read()读取单个字符
+		int single = 0;
+		while((single = reader.read()) > -1){
+			System.out.println((char)single);
+		}
+		reader.close();
+		
+		//读取多个字符
+		reader = new CharArrayReader(buf);
+		char[] cbuf = new char[1024];
+		int len = 0;
+		while((len = reader.read(cbuf)) > -1){
+			System.out.println(new String(cbuf,0,len));
+		}
+		reader.close();
+	}
+	
+	public void pipleReaderDemo() throws Exception{
+		
 	}
 }
